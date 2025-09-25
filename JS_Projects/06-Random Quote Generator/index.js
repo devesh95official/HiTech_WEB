b5e2a827d61f801703e4bd5ac982c216f6cf3a9c
Devesh Singh <devesh95official@gmail.com>
2025-09-25 11:31:19 +0530

    Add files via upload

---
+++ JS_Projects/06-Random Quote Generator/index.js
@@ -0,0 +1,33 @@
+// DOM Element
+const btnEl = document.getElementById("btn");
+const quoteEl = document.getElementById("quote");
+const autEl = document.getElementById("aut");
+
+const apiUrl = `https://api.api-ninjas.com/v1/quotes`;
+const apiKey = "usCdIMhtaN+YCismTz7hhQ==PrE3alofez6KGtUk";
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+};
+
+const getJoke = async () => {
+    try {
+        quoteEl.innerText = "Loading...";
+        const response = await fetch(apiUrl, options);
+        const data = await response.json();
+        const quote = data[0].quote;
+        const author = data[0].author;
+        console.log(quote);
+        quoteEl.innerText = quote;
+        autEl.innerText=author;
+    } catch (error) {
+        quoteEl.innerText = "An error happened, try again later.";
+        console.log(error);
+    }
+};
+getJoke();
+
+btnEl.addEventListener("click", getJoke);
